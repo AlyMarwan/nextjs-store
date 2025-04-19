5232b8ac3dae184c7278321b4236ec57d5612749
AlyMarwan <amarwan43@gmail.com>
2025-04-19 15:15:14 +0200

    added the signout button (Clerk)

--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,6 +1,19 @@
+'use client'
+
+import { SignOutButton } from "@clerk/nextjs"
+import Link from "next/link"
+import { toast } from "sonner"
+
+
 function SignOutLink() {
+  
+  const handleLogout = () =>{
+    toast('logout successful')
+  }
   return (
-    <div>SignOutLink</div>
+    <SignOutButton >
+      <Link href='/' className="w-full text-left" onClick={handleLogout}>Logout</Link>
+    </SignOutButton>
   )
 }
 export default SignOutLink
