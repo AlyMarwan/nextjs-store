c1195e1b8bd333f812790063fbedc91a7adaa352
AlyMarwan <amarwan43@gmail.com>
2025-04-24 21:16:28 +0200

    added an admin dashboard, set up an admin profile, admin dashboard only acessible to admins

---
+++ app/admin/Sidebar.tsx
@@ -0,0 +1,22 @@
+'use client'
+import { Button } from "@/components/ui/button"
+import { adminLinks } from "@/utils/links"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+function Sidebar() {
+  const pathname = usePathname()
+  console.log(pathname)
+  return (
+    <aside>
+      {adminLinks.map((link)=>{
+        const isActivePage = pathname === link.href
+        const variant = isActivePage? 'secondary' : 'ghost'
+        return <Button asChild className="w-full mb-2 capitalize font-normal justify-start" variant={variant} key={link.href}>
+          <Link key={link.href} href={link.href}>{link.label}</Link>
+        </Button>
+      })}
+    </aside>
+  )
+}
+export default Sidebar
---
+++ app/admin/products/page.tsx
@@ -0,0 +1,6 @@
+function page() {
+  return (
+    <div>Productspage</div>
+  )
+}
+export default page
